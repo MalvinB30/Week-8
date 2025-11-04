259bee793f9eefee58a467af7e0e46f2e106fd99
Malv jahanto <MalvinB30>
2025-11-04 18:58:15 +0700

    class CreditCardPayment and BankTransferPayment

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -1,5 +1,16 @@
 package Week8;
 
-public class CreditCardPayment {
+public class CreditCardPayment extends Payment {
+    private String cardNumber;
 
+    public CreditCardPayment(double amount, String cardNumber) {
+        super(amount);
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void processPayment() {
+        System.out.println("Processing credit card payment of $" + amount + " for card number " + cardNumber);
+    }
 }
+
